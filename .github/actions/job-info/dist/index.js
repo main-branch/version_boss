1eef87e87395cafdc896fa84fcbff55add8cf06a
James Couball <jcouball@yahoo.com>
2023-12-15 14:15:56 -0800

    Add Slack Notification step

--- .github/actions/job-info/dist/index.js
+++ .github/actions/job-info/dist/index.js
@@ -33528,23 +33528,25 @@ async function run() {
     const repository = core.getInput('repository') || github.context.repo.repo;
     const owner = github.context.repo.owner;
     const runId = core.getInput('run_id') || github.context.runId;
-    const jobName = core.getInput('job_name');
     const perPage = core.getInput('per_page') || 30;
 
     const octokit = github.getOctokit(token);
 
-    const response = await octokit.rest.actions.listJobsForWorkflowRun({
+    const jobs = await octokit.rest.actions.listJobsForWorkflowRun({
       owner,
       repo: repository,
       run_id: runId,
       per_page: parseInt(perPage)
-    });
+    }).data.jobs;
+
+    // Log the github context
+    core.info("\nGithub context:");
+    core.info(JSON.stringify(github.context, null, 2));
 
     // Log the jobs array
-    core.info("Jobs data:");
-    core.info(JSON.stringify(response.data.jobs, null, 2));
+    core.info("\n\nJobs data:");
+    core.info(JSON.stringify(jobs, null, 2));
 
-    const jobs = response.data.jobs;
     core.setOutput('jobs', jobs);
   } catch (error) {
     core.setFailed(error.message);
