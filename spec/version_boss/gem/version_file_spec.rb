d71ae876c6afb36bd0b7a015e982d56864adc08d
James Couball <jcouball@yahoo.com>
2024-09-12 15:35:20 -0700

    Update continuous integration and experimental ruby builds

--- spec/version_boss/gem/version_file_spec.rb
+++ spec/version_boss/gem/version_file_spec.rb
@@ -26,12 +26,21 @@ RSpec.describe VersionBoss::Gem::VersionFile do
     it do
       is_expected.to(
         have_attributes(
-          path: path,
-          content_before: content_before,
-          version: version,
-          content_after: content_after
+          path:,
+          content_before:,
+          version:,
+          content_after:
         )
       )
+    end
+
+    context 'when the version is not an IncrementableVersion' do
+      let(:version_file) { described_class.new(path, content_before, '2.4.0', content_after) }
+      subject { version_file }
+
+      it 'should raise a VersionBoss::Error' do
+        expect { subject }.to raise_error(VersionBoss::Error, 'version must be an IncrementableVersion')
+      end
     end
   end
 
@@ -68,6 +77,15 @@ RSpec.describe VersionBoss::Gem::VersionFile do
       version_file.version = new_version
       actual_content = File.read(path)
       expect(actual_content).to eq(expected_content)
+    end
+
+    context 'when the new version is not an IncrementableVersion' do
+      let(:new_version) { '9.9.9' }
+      it 'should raise a VersionBoss::Error' do
+        expect { version_file.version = new_version }.to(
+          raise_error(VersionBoss::Error, 'new_version must be an IncrementableVersion')
+        )
+      end
     end
   end
 end
--- version_boss.gemspec
+++ version_boss.gemspec
@@ -16,7 +16,7 @@ Gem::Specification.new do |spec|
 
   spec.homepage = 'http://github.com/main-branch/version_boss'
   spec.license = 'MIT'
-  spec.required_ruby_version = '>= 3.0.0'
+  spec.required_ruby_version = '>= 3.1.0'
 
   spec.metadata['allowed_push_host'] = 'https://rubygems.org'
 
@@ -35,6 +35,10 @@ Gem::Specification.new do |spec|
   spec.bindir = 'exe'
   spec.executables = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
+  spec.requirements = [
+    'Platform: Mac, Linux, or Windows',
+    'Ruby: MRI 3.1 or later, TruffleRuby 24 or later, or JRuby 9.4 or later'
+  ]
 
   spec.add_dependency 'thor', '~> 1.3'
 
